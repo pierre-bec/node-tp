80c174a88ca4549ee3790bb1d613bb4b876305b8
pbec <21092020>
2021-03-11 09:25:54 +0100

    cors

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ const jwt = require('jsonwebtoken')
 const passport = require('passport')
 const request = require("request");
 const secret = 'thisisverysecret';
+const cors = require('cors');
+
+app.use(cors());
 
 
 const JwtStrategy = require('passport-jwt').Strategy, ExtractJwt = require('passport-jwt').ExtractJwt;
